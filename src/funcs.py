6deff0df1041c764b9682f9a86da55080f89847c
luanzd <diegoluandenil@hotmail.com>
2026-09-21 19:30:30 -0300

    Adicionando derivadas exatas para teste de f2

--- src/funcs.py
+++ src/funcs.py
@@ -14,4 +14,25 @@ def f3(x):
 
 
 def f4(x):
-    return sin(x) * x + 4
+    return sin(x) * x + 4A
+
+#Derivadas para comparar com as númericas
+
+def df1(x):
+    return 8 * x**3 + 12 * x**2 + 6 * x - 10
+ 
+ 
+def df2(x):
+    return 5 * x**4 - 8 * x**3 - 27 * x**2 + 44 * x + 4
+ 
+ 
+def df3(x):
+    return 15 * x**2 + 2 * x + 2 * e ** (1 - 2 * x) - sin(x)
+ 
+ 
+def df4(x):
+    return sin(x) + x * cos(x)
+ 
+ 
+DERIVADAS = {f1: df1, f2: df2, f3: df3, f4: df4}
+
